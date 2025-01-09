bd126ad7d576f57b62520fdd0ac5f518e62eb219
srikanth_minde <srikanth9705377176@gmail.com>
2025-01-09 11:15:25 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,38 @@
+// var fs=require("fs");
+// var http=require("http");
+// var url=require("url");
+// http.createServer((req,res)=>{
+// if(req.url==="/srikanth"){
+// res.end("edupoly student")
+//   }
+
+// if(req.url==="/jjj"){
+// var b =fs.readFileSync("form.html")
+// res.end(b.toString())
+// }}).listen(6000)
+
+// var fs=require("fs")
+// var http = require("http")
+// var url = require("url")
+//  http.createServer((req,res)=>{
+//     if(req.url==="/srikanth"){
+//         res.end("Edupoly")
+//     }
+//     if(req.url==="/pqr"){
+//         var rr=fs.readFileSync("form.html")
+//         res.end(rr.toString())
+//     }}).listen(7500)
+
+
+
+//     var fs=require("fs")
+// var http = require("http")
+// var url = require("url")
+//  http.createServer((req,res)=>{
+//     if(req.url==="/favicon.ico"){
+//         res.end("Edupoly")
+//     }
+//     if(req.url==="/pqr"){
+//         var rr=fs.readFileSync(path.basename(req.url))
+//         res.end(rr.toString())
+//     }}).listen(7000)
