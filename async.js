67d5f66848b7c90a86377dc7a1cd32d2e89643e7
srikanth_minde <srikanth9705377176@gmail.com>
2025-01-10 13:15:11 +0530

    Add files via upload

---
+++ async.js
@@ -0,0 +1,28 @@
+// async function facto(n){
+//     var f=1
+//     if(isNaN(n)||(n<0)){
+//      return Promise.reject("Invalid Input")
+//     }
+//  else{
+//      for ( var i=n;i>=1;i--){
+//          f=f*i
+//      }
+//  }
+//      return f
+//   }
+//   facto(-8)
+//   .then(function(res){
+//          console.error(res)
+//       })
+//       .catch(function(err){
+//          console.error("Error ra edhi")   
+//      })
+ 
+//      console.log("Last Line")
+ 
+     //memory optimation
+     //suitable for asynchronus function management
+     //error handling easy
+     //asynchronus pattane lo promise
+
+     
