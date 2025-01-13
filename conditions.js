3f4a1716e8d35e9ce1f580043388b6830aa1e183
srikanth_minde <srikanth9705377176@gmail.com>
2025-01-13 20:41:20 +0530

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,23 @@
+//  function facto(n){
+//    var f=1
+//    if(isNaN(n)||(n<0)){
+//     return Promise.reject("Invalid Input")
+//    }
+// else{
+//     for ( var i=n;i>=1;i--){
+//         f=f*i
+//     }
+// }
+//     return Promise.resolve(f)
+//  }
+//  facto(5)
+//  .then(function(res){
+//         console.error(res)
+//      })
+//      .catch(function(err){
+//         console.error(err)   
+//     })
+
+//     console.log("Last Line")
+
+    //call back function lo promise
