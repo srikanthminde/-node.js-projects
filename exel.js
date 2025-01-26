36db23e273432a2c79053a76f8a723f9d2ed6adc
srikanth_minde <srikanth9705377176@gmail.com>
2025-01-26 21:17:58 +0530

    Add files via upload

---
+++ exel.js
@@ -0,0 +1,42 @@
+// var xl = require("excel4node")
+// var workbook = new xl.Workbook()
+
+// var worksheet1 = workbook.addWorksheet("sheet1")
+// var worksheet1 = workbook.addWorksheet("sheet1")
+// var worksheet1 = workbook.addWorksheet("sheet1")
+
+// var style = workbook.createStyle({
+//     font: {
+//       color: '#FF0800',
+//     },
+//     numberFormat: '$#,##0.00; ($#,##0.00); -',
+//   });
+  
+//   var style1 = workbook.createStyle({
+//     font: {
+//       color: '#FFFF00',
+//     },
+//     numberFormat: '$#,##0.00; ($#,##0.00); -',
+//   });
+  
+//   var style2 = workbook.createStyle({
+//     font: {
+//       color: '#00FF00',
+//     },
+//     numberFormat: '$#,##0.00; ($#,##0.00); -', 
+//   });
+  
+//   var style3 = workbook.createStyle({
+//     font: {
+//       color: '#FF00FF',
+//     },
+//     numberFormat: '$#,##0.00; ($#,##0.00); -',
+//   });
+  
+  
+// worksheet1.cell(1,1).string("Faruk bhai").style(style3)
+// worksheet1.cell(2,1).string("Pradeep").style(style)
+// worksheet1.cell(3,1).string("karthik").style(style1)
+// worksheet1.cell(4,1).string("Laxman").style(style2)
+
+// workbook.write("srikanth.xlsx")
