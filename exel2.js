4b1688ac633f807e4d74903a3da10a17e080078c
srikanth_minde <srikanth9705377176@gmail.com>
2025-01-30 10:26:55 +0530

    Add files via upload

---
+++ exel2.js
@@ -0,0 +1,46 @@
+// var xl = require("excel4node");
+// var workbook = new xl.Workbook();
+// var worksheet1 = workbook.addWorksheet("sheet1");
+
+// // Define styles
+// var style2 = workbook.createStyle({
+//   fill: {
+//     type: 'pattern',
+//     patternType: 'solid',
+//     fgColor: '#FF00FF'
+//   },
+//   font: {
+//     color: '#FFFFFF' 
+//   },
+//   font: {
+//     size: 10,             
+//     name: 'Monaco',          
+//     bold: true,   
+//     italic: true,                    
+//   } 
+// });
+
+// var style1 = workbook.createStyle({
+//   fill: {
+//     type: 'pattern',
+//     patternType: 'solid',
+//     fgColor: '#FFFF00'
+//   },
+//   font: { 
+//     color: '#FF00FF' 
+//   },
+ 
+//   font: {
+//     size: 10,             
+//     name: 'Old English',          
+//     bold: true,   
+//     italic: true,                    
+//   } 
+// });
+
+  
+
+// worksheet1.cell(1,1).string('Pradeepuuu').style(style2);  
+// worksheet1.cell(1,2).string('Laxmana').style(style1); 
+// workbook.write("srikanth.xlsx");  
+   
