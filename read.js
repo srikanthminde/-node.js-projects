f652260547a8f92b57abcba00f33ae533166de61
srikanth_minde <srikanth9705377176@gmail.com>
2025-02-07 10:29:37 +0530

    Add files via upload

---
+++ read.js
@@ -0,0 +1,147 @@
+// var fs=require("fs")
+// var x=fs.readFileSync('students.txt')
+// console.log(x.toString())
+
+
+// var fs=require("fs",n)
+// var n=fs.readFile('students.txt',(err,data)=>{
+// if(err){
+//     console.log(err)
+// }
+// else {
+//      console.log(data.toString())
+// }
+// })
+
+// var fs=require("fs").promises
+//     fs.readFile('students.txt')
+    
+//     .then((data)=>{
+//         console.log(data.toString())
+//     })
+    
+//     .catch((err)=>{
+//         console.log(err.message)
+//     })
+    
+
+
+// var fs=require("fs")
+// var s="sai baby srinu pradeep karthik ayyappa venky mama praveen prashanth bindhu"
+// fs.writeFile("orders.txt",s,err=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log("nothing")
+//     }
+// })
+
+// var fs=require("fs")
+// var m=" a b c d e f g h i "
+//   fs.writeFile("myfile.txt",m,err=>{
+//     if(err){
+//     console.log(err)
+
+//     }
+//     else{
+//         console.log("Nothing CMF 1")
+//     }
+//   })
+
+
+// var fs=require("fs")
+// fs.writeFileSync("Baby.txt","shruthi pooja deepika praveen venky mama")
+
+
+// var fs=require("fs")
+// fs.unlinkSync("life.txt")
+
+
+// var fs=require("fs")
+// var b="paddy yuriya unuka "
+//  fs.unlinkSync("my documents",b,err=>{
+//   if(err){
+//     console.log(err)
+
+//   }
+//   else{
+//     console.error("Iam Former")
+//   }
+// })
+
+
+
+// var fs=require("fs").promises
+// fs.unlink("life.txt")
+// .then((data)=>{
+//   console.log(data)
+// })
+// .catch((err)=>{
+//   console.log(err)
+// })
+
+// var fs=require("fs")
+//  fs.unlinkSync("life.txt")
+//  if(data){
+//   console.log(data)
+//  }
+//  else{
+//   console.log(err)
+//  }  
+
+
+// var fs=require("fs")
+// fs.renameSync("life.txt","Baby.txt")
+
+
+// var fs=require("fs")
+// fs.renameSync("institute.txt","Coder.txt",()=>{
+//   if(data){
+//     console.log(data)
+//   }
+//   else{
+// console.log(err)
+//   }
+// })
+
+// var fs=require("fs").promises
+// fs.rename("Coding.txt","Edupoly.txt")
+// .then((data)=>{
+//   console.log(data)
+// })
+// .catch((err)=>{
+//   console.log(err)
+// })
+
+// var fs=require("fs").promises
+// fs.appendFile("Edupoly.txt","pradep, karthik Srinu , ayyappa")
+// .then((data)=>{
+//   console.log(data)
+// })
+// .catch((err)=>{
+//   console.log(err)
+// })
+
+// var fs=require("fs")
+// fs.appendFileSync("Edupoly.txt","Karthik")
+
+// var fs=require("fs")
+// fs.appendFileSync("Edupoly.txt","Venky,mama, Nikhil,",()=>{
+//   if(data){
+//     console.log(data)
+//   }
+//   else{
+//     console.log(err)
+//   }
+// })
+
+// var fs=require("fs").promises
+// fs.appendFile("Edupoly.txt","Faruk , Praveen Gubbala")
+// .then((data)=>{
+//   console.log(data)
+// })
+// .catch((err)=>{
+//   console.log(err)
+// })
+ 
