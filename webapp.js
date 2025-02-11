6b793f35ada3108bc6631c1db56f7d41adad715b
srikanth_minde <srikanth9705377176@gmail.com>
2025-02-11 10:51:05 +0530

    Add files via upload

---
+++ webapp.js
@@ -0,0 +1,36 @@
+// var http=require("http")
+//  http.createServer(((req,res)=>{
+//     if(req.url==="/srikanth"){
+//     res.end("edupoly student")
+//     console.log(req.url)
+//        }
+//        if(req.url==="/abc"){
+//         res.end("manga")
+//         console.log(req.url)
+//        }
+//  })).listen(3700)
+
+
+    //     var fs=require("fs")
+    //     var http=require("http")
+    //     var path = require("path")
+    //     var  basename  = require("path")
+    //     var url=require("url")
+    //     http.createServer(((req,res)=>{
+    //    if(req.url==="/srikanth"){
+    //    res.end("edupoly student")
+    //       }
+
+    //     if(req.url==="/abc"){
+    //     var bhaii =fs.readFileSync("data.html")
+    //     // var vv=path.resolve("data.html")
+    //     res.end(bhaii.toString())
+    //    }
+    //    })).listen(3700)
+    //    console.log(url)
+
+
+  
+//    console.log(url)
+
+
